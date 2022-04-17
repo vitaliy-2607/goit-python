daa40a4e251227ba4b061c30c0108bf45c377da5
Vitaliy <76917071+vitaliy-2607@users.noreply.github.com>
2022-04-17 18:10:28 +0300

    m5

--- module5/task8.py
+++ module5/task8.py
@@ -3,8 +3,10 @@ grades = {"A": 5, "B": 5, "C": 4, "D": 3, "E": 3, "FX": 2, "F": 1}
 
 
 def formatted_grades(students):
+    list = []
     count = 1
     for key, value in students.items():
-        print('{:>4}{}{:<10}{}{:^5}{}{:^5}'.format(
+        list.append('{:>4}{}{:<10}{}{:^5}{}{:^5}'.format(
             count, '|', key, '|', value, '|', grades.get(value)))
         count += 1
+    return list
