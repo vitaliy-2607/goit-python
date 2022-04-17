0f942775cb5933efd96364c030eef91beeb88e80
Vitaliy <76917071+vitaliy-2607@users.noreply.github.com>
2022-04-17 09:51:10 +0300

    Create task7.py

---
+++ module5/task7.py
@@ -0,0 +1,13 @@
+# Напишите функцию translate, которая проводит транслитерацию кириллического алфавита на латинский.
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "u", "ja", "je", "ji", "g")
+
+TRANS = {}
+for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(c)] = l
+    TRANS[ord(c.upper())] = l.upper()
+
+
+def translate(name):
+    return name.translate(TRANS)
