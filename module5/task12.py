a23bc6624bf1619512ffa471a65b27fc42597f0d
Vitaliy <76917071+vitaliy-2607@users.noreply.github.com>
2022-04-23 07:39:49 +0300

    m5

--- module5/task12.py
+++ module5/task12.py
@@ -11,6 +11,6 @@ import re
 def replace_spam_words(text, spam_words):
     for spam in spam_words:
         clear_words = '*' * len(spam)
-        result = re.sub(spam, spam_words, text, flags=re.IGNORECASE)
+        result = re.sub(spam, clear_words, text, flags=re.IGNORECASE)
         text = result
     return text
